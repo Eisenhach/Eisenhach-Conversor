d4b2ad65eab33da23cce0d426134d991b051cb25
Tomas Eisenhach Pablo <eisenhach.tomas@gmail.com>
2023-04-12 19:13:35 -0300

    añadido nuevo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-function convertir() {
-    const cantidad = parseFloat(document.getElementById("cantidad").value);
-    const tasaDeCambio = 209.41;
-    const resultado = cantidad * tasaDeCambio;
-    
-    document.getElementById("resultado").value = resultado.toFixed(2);
+const boton = document.getElementById("boton");
+const contador = document.getElementById("contador");
+
+let clicks = 0;
+
+boton.addEventListener("click", contarClicks);
+
+function contarClicks() {
+  clicks++;
+
+  if (clicks <= 100) {
+    contador.textContent = `Has hecho ${clicks} clicks`;
+  } else {
+    contador.textContent = "Has llegado al límite de clicks";
+    boton.removeEventListener("click", contarClicks);
   }
-  
+}
+
+function reiniciarContador() {
+  clicks = 0;
+  contador.textContent = "";
+  boton.addEventListener("click", contarClicks);
+}
